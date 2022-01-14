5563fdde58a04f37d31b0623d60700d34bc4abae
Jebbit-koi <75734684+Jebbit-koi@users.noreply.github.com>
2022-01-14 21:46:47 +0900

    form and connecting firebase

--- mini-router/src/pages/NewMeetup.js
+++ mini-router/src/pages/NewMeetup.js
@@ -1,5 +1,24 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
-  return <div>New Meetup Page</div>;
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-mini-router-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupPage;
